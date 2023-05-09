74a78284d33baaf880caf0b12346605fa22c2410
Crowdbotics <team@crowdbotics.com>
2023-05-09 14:33:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-fdadsale-app-40582",
+  url: "https://tst-fdadsale-app-40582.botics.co",
+  api: "https://tst-fdadsale-app-40582.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
